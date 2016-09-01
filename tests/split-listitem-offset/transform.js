d7e6c6cbf1bb99324715f9ea2a53f9b07d82c3ff
Soreine <soreine.plume@gmail.com>
2016-09-01 11:22:39 +0200

    Use GitBook Eslint config, and lint.

--- tests/split-listitem-offset/transform.js
+++ tests/split-listitem-offset/transform.js
@@ -1,13 +1,13 @@
 
 module.exports = function(plugin, state) {
-    let p = state.document.findDescendant(node => node.type == 'paragraph');
+    const p = state.document.findDescendant(node => node.type == 'paragraph');
 
-    let withCursor = state.transform()
+    const withCursor = state.transform()
         .collapseToStartOf(p)
         .moveToOffsets(5, 5)
         .apply();
 
-    let transform = withCursor.transform();
+    const transform = withCursor.transform();
     return plugin.transforms
         .splitListItem(transform)
         .apply();
--- tests/split-listitem-start/transform.js
+++ tests/split-listitem-start/transform.js
@@ -1,12 +1,12 @@
 
 module.exports = function(plugin, state) {
-    let p = state.document.findDescendant(node => node.type == 'paragraph');
+    const p = state.document.findDescendant(node => node.type == 'paragraph');
 
-    let withCursor = state.transform()
+    const withCursor = state.transform()
         .collapseToStartOf(p)
         .apply();
 
-    let transform = withCursor.transform();
+    const transform = withCursor.transform();
     return plugin.transforms
         .splitListItem(transform)
         .apply();
