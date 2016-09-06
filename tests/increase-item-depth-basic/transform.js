b98111d65e42d7b9886fd9a4a4b9407bd68423e8
Soreine <soreine.plume@gmail.com>
2016-09-06 11:06:20 +0200

    Rename and cleanup increase-item-depth tests

--- tests/increase-item-depth-basic/transform.js
+++ tests/increase-item-depth-basic/transform.js
@@ -1,12 +1,9 @@
 
 module.exports = function(plugin, state) {
-    console.log('test');
     const selectedBlock = state.document.getDescendant('_selection_key');
-    let transform = state.transform();
+    const transform = state.transform();
     state = transform.moveToRangeOf(selectedBlock).apply();
 
-    console.log(state);
-    
     return plugin.transforms.increaseItemDepth(state.transform())
         .apply();
 };
