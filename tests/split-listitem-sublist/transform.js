9b459ff1e75810b9abbf1993317728f84ebf94c7
Soreine <soreine.plume@gmail.com>
2016-09-07 17:29:09 +0200

    Add test for split item with sublist

---
+++ tests/split-listitem-sublist/transform.js
@@ -0,0 +1,14 @@
+
+module.exports = function(plugin, state) {
+    const selectedBlock = state.document.getDescendant('_selection_key');
+
+    const withCursor = state.transform()
+              .collapseToStartOf(selectedBlock)
+              .moveForward(2) // It|em 1
+              .apply();
+
+    const transform = withCursor.transform();
+    return plugin.transforms
+        .splitListItem(transform)
+        .apply();
+};
