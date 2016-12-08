6a68374998a59b5a51c4dedfd3d398f8c986c24b
Soreine <soreine.plume@gmail.com>
2016-12-08 10:32:43 +0100

    Fix GitbookIO/editor#428 and fix #13

---
+++ tests/backspace-empty-between-inline/transform.js
@@ -0,0 +1,22 @@
+const expect = require('expect');
+
+module.exports = function(plugin, state) {
+    const selectedBlock = state.document.getDescendant('_selection_key');
+    state = state.transform()
+        .collapseToStartOf(selectedBlock).apply();
+
+    state = plugin.onKeyDown(
+        {
+            preventDefault: () => {},
+            stopPropagation: () => {}
+        },
+        { key: 'backspace' },
+        state
+    );
+
+    // Selection check
+    // expect(state.startBlock.text).toEqual('');
+    // expect(state.selection.anchorOffset).toEqual(0);
+    // expect(state.selection.isCollapsed).toBe(true);
+    return state;
+};
