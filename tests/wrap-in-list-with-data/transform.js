597d88ddf2275797756478a62e6bf3f654c9e803
Soreine <soreine.plume@gmail.com>
2017-05-12 16:26:55 +0200

    Remove legacy UL and OL options types

--- tests/wrap-in-list-with-data/transform.js
+++ tests/wrap-in-list-with-data/transform.js
@@ -2,6 +2,6 @@
 module.exports = function(plugin, state) {
     const transform = state.transform();
     const data = { style: { listStyleType: 'decimal' } };
-    return plugin.transforms.wrapInList(transform, true, data)
+    return plugin.transforms.wrapInList(transform, 'ol_list', data)
         .apply();
 };
--- tests/wrap-in-ol/transform.js
+++ tests/wrap-in-ol/transform.js
@@ -1,6 +1,6 @@
 
 module.exports = function(plugin, state) {
     const transform = state.transform();
-    return plugin.transforms.wrapInList(transform, true)
+    return plugin.transforms.wrapInList(transform, 'ol_list')
         .apply();
 };
