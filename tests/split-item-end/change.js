7f604512d9ceb81936275428e97b42d07beae2bb
Quentin Calvez <quentin.calvez@quentez.com>
2018-08-25 18:08:12 -0700

    :arrow_up: Upgrade more deprecated methods.

--- tests/split-item-end/change.js
+++ tests/split-item-end/change.js
@@ -2,6 +2,6 @@ export default function(plugin, change) {
     const { value } = change;
     const p = value.document.findDescendant(node => node.type == 'paragraph');
 
-    change.collapseToEndOf(p);
+    change.moveToEndOfNode(p);
     return plugin.changes.splitListItem(change);
 }
--- tests/split-item-offset-multiple-blocks/change.js
+++ tests/split-item-offset-multiple-blocks/change.js
@@ -2,6 +2,10 @@ export default function(plugin, change) {
     const { value } = change;
     const p = value.document.findDescendant(node => node.type == 'paragraph');
 
-    change.moveToStartOfNode(p).moveOffsetsTo(5, 5);
+    change
+        .moveToStartOfNode(p)
+        .moveAnchorTo(5)
+        .moveFocusTo(5);
+
     return plugin.changes.splitListItem(change);
 }
