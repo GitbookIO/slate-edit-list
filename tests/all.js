f407bba782aea716f67d242d0545d6c3e23db05a
Soreine <soreine.plume@gmail.com>
2018-09-09 10:08:55 +0200

    Start using slate-hyperscript

--- tests/all.js
+++ tests/all.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 import expect from 'expect';
 import fs from 'fs';
 import path from 'path';
 import Slate from 'slate';
-import readMetadata from 'read-metadata';
 
 import EditList from '../lib';
 
@@ -26,12 +27,11 @@ describe('slate-edit-list', () => {
             const plugin = EditList();
 
             const dir = path.resolve(__dirname, test);
-            const input = readMetadata.sync(path.resolve(dir, 'input.yaml'));
-            const expectedPath = path.resolve(dir, 'expected.yaml');
+            const input = require(path.resolve(dir, 'input.js')).default;
+            const expectedPath = path.resolve(dir, 'expected.js');
             const expected =
-                fs.existsSync(expectedPath) && readMetadata.sync(expectedPath);
+                fs.existsSync(expectedPath) && require(expectedPath).default;
 
-            // eslint-disable-next-line
             const runChange = require(path.resolve(dir, 'change.js')).default;
 
             const valueInput = deserializeValue(plugin, input);
